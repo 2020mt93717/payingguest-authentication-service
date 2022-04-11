348a829cd1b8cd8ace59f2af37117fe7f17f800d
Jaikarthik Natarajan <2020mt93717@wilp.bits-pilani.ac.in>
2022-04-11 17:43:02 +0530

    Initial Draft of Authentication Service.

---
+++ src/main/java/payingguest/authentication/controller/AuthenticationController.java
@@ -0,0 +1,66 @@
+/***************************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2022 2020mt93717
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * **************************************************************************************/
+
+package payingguest.authentication.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import payingguest.authentication.domain.User;
+import payingguest.authentication.service.AuthenticationService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService mAuthenticationService;
+
+    @PostMapping(
+            value = "/login",
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    public ResponseEntity<String> login(@RequestBody User pUser) {
+        // Compare user and password hash in DB. If yes then generate token else respond with error
+        String token = mAuthenticationService.generateToken(pUser.getUserName());
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+
+    @PostMapping(
+            value = "/register",
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    public ResponseEntity<String> register(@RequestBody User pUser) {
+        // Persist user
+
+        return new ResponseEntity<>("Registered", HttpStatus.OK);
+    }
+
+}
